90323d5da9a39dae6d9b0c67bd4d16ffc7cdeb0c
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-01-05 06:28:51 -0800

    add kafka message producer

--- src/KafkaSample.Producer/Program.cs
+++ src/KafkaSample.Producer/Program.cs
@@ -2,9 +2,22 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using Confluent.Kafka;
 using KafkaSample.Producer;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton((IServiceProvider provider) =>
+{
+  ProducerConfig config = builder.Configuration.GetSection("Kafka").Get<ProducerConfig>() ??
+                          throw new Exception("No kafka producer config section: 'Kafka'.");
+  IProducer<Null, string> producer = new ProducerBuilder<Null, string>(config).Build();
+
+  ILogger<KafkaMessageProducer> logger = provider.GetRequiredService<ILogger<KafkaMessageProducer>>();
+
+  KafkaMessageProducer messageProducer = new(producer, logger);
+
+  return messageProducer;
+});
 builder.Services.AddHostedService<ProducerHostedService>();
 
 WebApplication app = builder.Build();
