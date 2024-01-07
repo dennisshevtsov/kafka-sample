f36f683e031ef9897aa600461a615156bd19263a
Dennis Shevtsov <dennis.shevtsov.dev@gmail.com>
2024-01-07 07:39:35 -0800

    implement consumer

--- src/KafkaSample.Consumer/Program.cs
+++ src/KafkaSample.Consumer/Program.cs
@@ -2,10 +2,27 @@
 // Licensed under the MIT License.
 // See LICENSE in the project root for license information.
 
+using Confluent.Kafka;
 using KafkaSample.Consumer;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHostedService<ConsumerHostedService>();
+builder.Services.AddHostedService((IServiceProvider provider) =>
+      {
+        ConsumerConfig config = builder.Configuration.GetSection("Kafka").Get<ConsumerConfig>() ??
+                                throw new Exception("No kafka consumer config section: 'Kafka'.");
+        IConsumer<Null, string> consumer = new ConsumerBuilder<Null, string>(config).Build();
+        string topic = builder.Configuration.GetValue<string>("LISTENING_TOPIC") ??
+                       throw new Exception("No listening kafka topic: 'LISTENING_TOPIC'.");
+        ILogger<ConsumerHostedService> logger = provider.GetRequiredService<ILogger<ConsumerHostedService>>();
+        ConsumerHostedService backbroundService = new
+        (
+          consumer: consumer,
+          topic   : topic,
+          logger  : logger
+        );
+
+        return backbroundService;
+      });
 
 WebApplication app = builder.Build();
 app.MapGet("/", () => "Kafka sample consumer working...");
